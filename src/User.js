7bed58f694a096d66fc31e316f38c3793e337e52
vitor-driven <vitor_henrique001@hotmail.com>
2023-05-08 09:30:18 -0300

    data-test

--- src/User.js
+++ src/User.js
@@ -4,7 +4,7 @@ export default function User() {
     const [userName, changeName] = useState("catanacomics");
     const askName = () => {
         const newName = prompt("Defina o novo nome de usuário:");
-        if (newName.length > 0) {
+        if (newName.length > 0 && newName !== " ") {
             changeName(newName);
         }
     };
@@ -12,18 +12,27 @@ export default function User() {
     const [userPic, changePic] = useState("assets/img/catanacomics.svg");
     const askPic = () => {
         const newPic = prompt("Link da nova foto de perfil:");
-        if (newPic.length > 0) {
+        if (newPic.length > 0 && newPic !== " ") {
             changePic(newPic);
         }
     };
 
     return (
         <div className="usuario">
-            <img src={userPic} onClick={askPic} alt="imagem de perfil" />
+            <img
+                src={userPic}
+                onClick={askPic}
+                data-test="profile-image"
+                alt="imagem de perfil"
+            />
             <div className="texto">
                 <span>
-                    <strong>{userName}</strong>
-                    <ion-icon name="pencil" onClick={askName}></ion-icon>
+                    <strong data-test="name">{userName}</strong>
+                    <ion-icon
+                        name="pencil"
+                        data-test="edit-name"
+                        onClick={askName}
+                    ></ion-icon>
                 </span>
             </div>
         </div>

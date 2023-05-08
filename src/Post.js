1c52eb7327cbdbed6a43f127c4e509cce53bde03
vitor-driven <vitor_henrique001@hotmail.com>
2023-05-08 09:00:27 -0300

    like e save post

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,20 @@
+import React, { useState } from "react";
+
 export default function Post(props) {
+    let initCount = Number(props.likeCount);
+    const [isLiked, setLike] = useState(false);
+    const toggleLike = () => {
+        setLike(!isLiked);
+    };
+
+    let userLike = isLiked ? 1 : 0;
+    let likeCount = initCount + userLike;
+
+    const [isSaved, setSave] = useState(false);
+    const toggleSave = () => {
+        setSave(!isSaved);
+    };
+
     return (
         <div className="post" data-test="post">
             <div className="topo">
@@ -11,19 +27,48 @@ export default function Post(props) {
                 </div>
             </div>
 
-            <div className="conteudo">
+            <div
+                onClick={toggleLike}
+                data-test="post-image"
+                className="conteudo"
+            >
                 <img src={props.postImage} alt={props.postUser} />
             </div>
 
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        {isLiked ? (
+                            <ion-icon
+                                data-test="like-post"
+                                name="heart"
+                                onClick={toggleLike}
+                                style={{ color: "red" }}
+                            ></ion-icon>
+                        ) : (
+                            <ion-icon
+                                data-test="like-post"
+                                name="heart-outline"
+                                onClick={toggleLike}
+                            ></ion-icon>
+                        )}
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        {isSaved ? (
+                            <ion-icon
+                                data-test="save-post"
+                                name="bookmark"
+                                onClick={toggleSave}
+                            ></ion-icon>
+                        ) : (
+                            <ion-icon
+                                data-test="save-post"
+                                name="bookmark-outline"
+                                onClick={toggleSave}
+                            ></ion-icon>
+                        )}
                     </div>
                 </div>
 
@@ -31,7 +76,7 @@ export default function Post(props) {
                     <img src={props.likePic} alt={props.likeUser} />
                     <div className="texto">
                         Curtido por <strong>adorable_animals</strong> e{" "}
-                        <strong>outras {props.likeCount} pessoas</strong>
+                        <strong>outras {likeCount} pessoas</strong>
                     </div>
                 </div>
             </div>
